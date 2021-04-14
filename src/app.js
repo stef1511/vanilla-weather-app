22c0b940676d0923ecc8968f9f9888f2fa168edc
stef1511 <stefanie.pemb@gmail.com>
2021-04-14 14:58:40 -0400

    Added function to display current temperature

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+function displayTemperature (response) {
+    console.log(response.data);
+    let temperature = Math.round (response.data.main.temp);
+    let currentTemperature = document.querySelector("#current-temperature");
+    currentTemperature.innerHTML = temperature;
+}
+
 let apiKey = "02e7aa00c7ab6f28f29780bb9858077e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q
+=London&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
