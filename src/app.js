6cac5add0cdfce02485b2af0babd644ce2077093
stef1511 <stefanie.pemb@gmail.com>
2021-04-15 13:21:05 -0400

    Added current date and time

--- src/app.js
+++ src/app.js
@@ -1,12 +1,45 @@
 function displayTemperature (response) {
     console.log(response.data);
-    let temperature = Math.round (response.data.main.temp);
+    let cityName = document.querySelector("#city");
+    cityName.innerHTML = response.data.name;
+    let skyCondition = document.querySelector("#description");
+    skyCondition.innerHTML = response.data.weather[0].description;
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = `${response.data.main.humidity}%`;
+     let wind = document.querySelector("#wind");
+     wind.innerHTML = Math.round(response.data.wind.speed);
     let currentTemperature = document.querySelector("#current-temperature");
-    currentTemperature.innerHTML = temperature;
+    currentTemperature.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = "02e7aa00c7ab6f28f29780bb9858077e";
+let city = "Palm Springs"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q
-=London&appid=${apiKey}&units=metric`;
+=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
+
+
+let now = new Date();
+
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let currentDate = document.querySelector("#date");
+currentDate.innerHTML = `${day} ${hour}:${minutes}`;
