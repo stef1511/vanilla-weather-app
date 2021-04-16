4db2be0afb261b33f8ede863f8fd7bb2ac13cb03
stef1511 <stefanie.pemb@gmail.com>
2021-04-16 17:00:19 -0400

    Added city-search function in JS

--- src/app.js
+++ src/app.js
@@ -1,26 +1,3 @@
-function displayTemperature (response) {
-    console.log(response.data);
-    let cityName = document.querySelector("#city");
-    cityName.innerHTML = response.data.name;
-    let skyCondition = document.querySelector("#description");
-    skyCondition.innerHTML = response.data.weather[0].description;
-    let humidity = document.querySelector("#humidity");
-    humidity.innerHTML = `${response.data.main.humidity}%`;
-     let wind = document.querySelector("#wind");
-     wind.innerHTML = Math.round(response.data.wind.speed);
-    let currentTemperature = document.querySelector("#current-temperature");
-    currentTemperature.innerHTML = Math.round(response.data.main.temp);
-    let icon = document.querySelector("#icon");
-     icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
-}
-
-let apiKey = "02e7aa00c7ab6f28f29780bb9858077e";
-let city = "Palm Springs"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q
-=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
-
 
 let now = new Date();
 let hour = now.getHours();
@@ -43,3 +20,45 @@ let days = [
 let day = days[now.getDay()];
 let currentDate = document.querySelector("#date");
 currentDate.innerHTML = `${day} ${hour}:${minutes}`;
+
+
+function search (city) {
+   let apiKey = "02e7aa00c7ab6f28f29780bb9858077e";
+   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q
+=${city}&appid=${apiKey}&units=metric`;
+   console.log(apiUrl);
+   axios.get(apiUrl).then(displayTemperature);
+
+}
+
+function handleSubmit (event) {
+    event.preventDefault();
+    let cityInput = document.querySelector("#city-input");
+    search(cityInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Palm Springs");
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityName = document.querySelector("#current-city");
+  cityName.innerHTML = response.data.name;
+  let skyCondition = document.querySelector("#description");
+  skyCondition.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.main.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let currentTemperature = document.querySelector("#current-temperature");
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
+
+
