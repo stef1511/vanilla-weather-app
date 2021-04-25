45b8c0d9ffbfa28832d1c07e1aaa74808da65cf6
stef1511 <stefanie.pemb@gmail.com>
2021-04-24 20:08:02 -0400

    Added API for forecast

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ let days = [
   "Friday",
   "Saturday",
 ];
+
 let day = days[now.getDay()];
 let currentDate = document.querySelector("#date");
 currentDate.innerHTML = `${day} ${hour}:${minutes}`;
@@ -42,14 +43,14 @@ form.addEventListener("submit", handleSubmit);
 
 search("Palm Springs");
 
-function displayForecast () {
+function displayForecast () { 
+
     let forecastElement = document.querySelector("#forecast");
    
     let days = ["Fri", "Sat", "Sun", "Mon"];
       let forecastHTML = `<div class="row">`;
     days.forEach(function(day){
         
-
     forecastHTML = forecastHTML + `
             <div class="col-2">
                 <span class= "forecast-day">
@@ -67,12 +68,19 @@ function displayForecast () {
         </div>`;
 
     });
-       
+    
         forecastHTML = forecastHTML + `</div>`;
         forecastElement.innerHTML = forecastHTML;
-
 }
+displayForecast();
 
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "02e7aa00c7ab6f28f29780bb9858077e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}
+    &lon=${coordinates.lon}&appid=${apiKey}`;
+    console.log(apiUrl);
+}
 function displayTemperature(response) {
   console.log(response.data);
   celsiusTemp = response.data.main.temp;
@@ -93,9 +101,8 @@ function displayTemperature(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+getForecast(response.data.coord);
 }
-
-displayForecast();
 
 function displayFahrenheit(event) {
     event.preventDefault();
